73a101c6f54f79c3756cd7d4ddf69e079c90bc2f
StevenMayo <steven.mayo@dayshape.com>
2024-03-19 11:33:29 +0000

    WIP

--- src/classes/Gantt.ts
+++ src/classes/Gantt.ts
@@ -1,6 +1,9 @@
+import { DateTime } from 'luxon';
 import { GanttHorizon } from './GanttHorizon';
 import { GanttRow } from './GanttRow';
-import type { GanttWorker } from './Worker';
+import { Task } from './Task';
+import { getRandom } from './Utility';
+import { GanttWorker } from './Worker';
 import { WorkHours } from './Workhours';
 
 export class Gantt {
@@ -15,6 +18,164 @@ export class Gantt {
 		this.horizon = new GanttHorizon();
 		this.workHours = new WorkHours();
 		this.workDays = [1, 2, 3, 4, 5];
+	}
+
+	render() {
+		const dateDiv = document.getElementsByClassName('dates')[0];
+
+		const monthRow = document.createElement('div');
+		monthRow.classList.add('flex', 'flex-row', 'border-b');
+
+		const weekRow = document.createElement('div');
+		weekRow.classList.add('flex', 'flex-row', 'border-b');
+
+		const dayRow = document.createElement('div');
+		dayRow.classList.add('flex', 'flex-row', 'border-b');
+
+		const hourRow = document.createElement('div');
+		hourRow.classList.add('flex', 'flex-row', 'border-t');
+		let previousDate = null;
+		let dayWidth = this.pixelsPerHour;
+		let weekWidth = this.pixelsPerHour;
+		let monthWidth = this.pixelsPerHour;
+
+		const ganttCanvas = document.getElementById('ganttCanvas') as HTMLCanvasElement;
+
+		ganttCanvas.style.width = `${this.width()}px`;
+		ganttCanvas.style.height = `${this.height()}px`;
+		// Set actual size in memory (scaled to account for extra pixel density).
+		const scale = window.devicePixelRatio; // Change to 1 on retina screens to see blrry canvas.
+		ganttCanvas.width = Math.floor(this.width() * scale);
+		ganttCanvas.height = Math.floor(this.height() * scale);
+
+		const ctx = ganttCanvas.getContext('2d') as CanvasRenderingContext2D;
+
+		// Normalize coordinate system to use CSS pixels.
+		ctx.scale(scale, scale);
+
+		ctx.strokeStyle = '#F4F7F5';
+		ctx.lineWidth = 1;
+
+		let currentX = -0.5;
+
+		for (let i = 0; i < this.horizon.hours(); i++) {
+			const newDate = this.horizon.start.plus({ hours: i });
+
+			if (!previousDate) {
+				previousDate = newDate;
+				continue;
+			}
+
+			if (previousDate.month !== newDate.month) {
+				const monthDiv = document.createElement('div');
+				monthDiv.innerText = `${previousDate.monthLong} ${previousDate.year}`;
+				monthDiv.classList.add('border-l', 'py-1');
+				monthDiv.style.width = `${monthWidth}px`;
+				monthRow.appendChild(monthDiv);
+				monthWidth = 0;
+			}
+
+			if (previousDate.weekNumber !== newDate.weekNumber) {
+				const weekDiv = document.createElement('div');
+				weekDiv.innerText = previousDate.weekNumber.toString();
+				weekDiv.classList.add('border-l', 'py-1');
+				weekDiv.style.width = `${weekWidth}px`;
+				weekRow.appendChild(weekDiv);
+				weekWidth = 0;
+			}
+
+			if (previousDate.day !== newDate.day) {
+				if (!this.workDays.includes(previousDate.weekday)) {
+					ctx.fillStyle = '#556162';
+					ctx.beginPath();
+
+					ctx.fillRect(currentX, 0, dayWidth, ganttCanvas.height);
+				}
+
+				currentX += dayWidth;
+
+				ctx.beginPath();
+				ctx.moveTo(currentX, 0);
+				ctx.lineTo(currentX, ganttCanvas.height);
+				ctx.stroke();
+
+				const dayDiv = document.createElement('div');
+				dayDiv.innerText = previousDate.day.toString();
+				dayDiv.classList.add('border-l', 'py-1');
+				dayDiv.style.width = `${dayWidth}px`;
+				dayRow.appendChild(dayDiv);
+				dayWidth = 0;
+			}
+
+			if (this.pixelsPerHour >= 24) {
+				const hourDiv = document.createElement('div');
+
+				hourDiv.innerText = newDate.hour.toString();
+				hourDiv.classList.add('border-l', 'py-1');
+				hourDiv.style.width = `${this.pixelsPerHour}px`;
+				hourRow.appendChild(hourDiv);
+			}
+
+			dayWidth += this.pixelsPerHour;
+			weekWidth += this.pixelsPerHour;
+			monthWidth += this.pixelsPerHour;
+			previousDate = newDate;
+		}
+
+		dateDiv?.appendChild(monthRow);
+		dateDiv?.appendChild(weekRow);
+		dateDiv?.appendChild(dayRow);
+
+		if (this.pixelsPerHour >= 24) {
+			dateDiv?.appendChild(hourRow);
+		}
+
+		const now = DateTime.now();
+		const nowX = Math.floor(now.diff(this.horizon.start, 'hours').hours * this.pixelsPerHour);
+
+		ctx.strokeStyle = '#FF3366';
+		ctx.lineWidth = 2;
+		ctx.beginPath();
+		ctx.moveTo(nowX, 0);
+		ctx.lineTo(nowX, ganttCanvas.height);
+		ctx.stroke();
+
+		const nowDot = document.createElement('div');
+		nowDot.classList.add('now-dot');
+		nowDot.style.marginLeft = `${nowX - 7.5}px`;
+		dateDiv.appendChild(nowDot);
+
+		return this;
+	}
+
+	generateWorkers(numWorkers = 100) {
+		for (let i = 1; i <= numWorkers; i++) {
+			const worker = new GanttWorker(`worker-${i}`);
+
+			for (let j = 1; j < getRandom(1, 20); j++) {
+				let start = this.horizon.start.plus({
+					hour: getRandom(1, this.horizon.hours()),
+				});
+
+				if (!this.workDays.includes(start.weekday)) {
+					start = start.set({ weekNumber: start.weekNumber + 1, weekday: 1 });
+				}
+
+				const duration = getRandom(8, 48);
+				let end = start.plus({ hour: duration });
+
+				if (!this.workDays.includes(end.weekday)) {
+					start = start.set({ weekday: 3 });
+					end = start.plus({ hour: duration });
+				}
+
+				worker.addTask(new Task(j, `Task ${j}`, start, end));
+			}
+
+			this.addWorker(worker);
+		}
+
+		return this;
 	}
 
 	addWorker(worker: GanttWorker) {
@@ -33,3 +194,4 @@ export class Gantt {
 		return this.rows.length * 28.8;
 	}
 }
+
--- src/main.ts
+++ src/main.ts
@@ -1,167 +1,9 @@
-import { DateTime } from 'luxon';
 import { Gantt } from './classes/Gantt';
-import { Task } from './classes/Task';
-import { GanttWorker } from './classes/Worker';
 import './style.css';
-
-function getRandom(min: number, max: number) {
-	return Math.random() * (max - min) + min;
-}
 
 const gantt = new Gantt();
 
-for (let i = 1; i <= 100; i++) {
-	const worker = new GanttWorker(`worker-${i}`);
-
-	for (let j = 1; j < getRandom(1, 20); j++) {
-		let start = gantt.horizon.start.plus({
-			hour: getRandom(1, gantt.horizon.hours()),
-		});
-
-		if (!gantt.workDays.includes(start.weekday)) {
-			start = start.set({ weekNumber: start.weekNumber + 1, weekday: 1 });
-		}
-
-		const duration = getRandom(8, 48);
-		let end = start.plus({ hour: duration });
-
-		if (!gantt.workDays.includes(end.weekday)) {
-			start = start.set({ weekday: 3 });
-			end = start.plus({ hour: duration });
-		}
-
-		worker.addTask(new Task(j, `Task ${j}`, start, end));
-	}
-
-	gantt.addWorker(worker);
-}
-
-const dateDiv = document.getElementsByClassName('dates')[0];
-
-const monthRow = document.createElement('div');
-monthRow.classList.add('flex', 'flex-row', 'border-b');
-
-const weekRow = document.createElement('div');
-weekRow.classList.add('flex', 'flex-row', 'border-b');
-
-const dayRow = document.createElement('div');
-dayRow.classList.add('flex', 'flex-row', 'border-b');
-
-const hourRow = document.createElement('div');
-hourRow.classList.add('flex', 'flex-row', 'border-t');
-let previousDate = null;
-let dayWidth = gantt.pixelsPerHour;
-let weekWidth = gantt.pixelsPerHour;
-let monthWidth = gantt.pixelsPerHour;
-
-const ganttDiv = document.getElementById('ganttDiv') as HTMLDivElement;
-const ganttCanvas = document.getElementById('ganttCanvas') as HTMLCanvasElement;
-
-ganttCanvas.style.width = `${gantt.width()}px`;
-ganttCanvas.style.height = `${gantt.height()}px`;
-// Set actual size in memory (scaled to account for extra pixel density).
-const scale = window.devicePixelRatio; // Change to 1 on retina screens to see blrry canvas.
-ganttCanvas.width = Math.floor(gantt.width() * scale);
-ganttCanvas.height = Math.floor(gantt.height() * scale);
-
-const ctx = ganttCanvas.getContext('2d') as CanvasRenderingContext2D;
-
-// Normalize coordinate system to use CSS pixels.
-ctx.scale(scale, scale);
-
-ctx.strokeStyle = '#F4F7F5';
-ctx.lineWidth = 1;
-
-let currentX = -0.5;
-
-for (let i = 0; i < gantt.horizon.hours(); i++) {
-	const newDate = gantt.horizon.start.plus({ hours: i });
-
-	if (!previousDate) {
-		previousDate = newDate;
-		continue;
-	}
-
-	if (previousDate.month !== newDate.month) {
-		const monthDiv = document.createElement('div');
-		monthDiv.innerText = `${previousDate.monthLong} ${previousDate.year}`;
-		monthDiv.classList.add('border-l', 'py-1');
-		monthDiv.style.width = `${monthWidth}px`;
-		monthRow.appendChild(monthDiv);
-		monthWidth = 0;
-	}
-
-	if (previousDate.weekNumber !== newDate.weekNumber) {
-		const weekDiv = document.createElement('div');
-		weekDiv.innerText = previousDate.weekNumber.toString();
-		weekDiv.classList.add('border-l', 'py-1');
-		weekDiv.style.width = `${weekWidth}px`;
-		weekRow.appendChild(weekDiv);
-		weekWidth = 0;
-	}
-
-	if (previousDate.day !== newDate.day) {
-		if (!gantt.workDays.includes(previousDate.weekday)) {
-			ctx.fillStyle = '#556162';
-			ctx.beginPath();
-
-			ctx.fillRect(currentX, 0, dayWidth, ganttCanvas.height);
-		}
-
-		currentX += dayWidth;
-
-		ctx.beginPath();
-		ctx.moveTo(currentX, 0);
-		ctx.lineTo(currentX, ganttCanvas.height);
-		ctx.stroke();
-
-		const dayDiv = document.createElement('div');
-		dayDiv.innerText = previousDate.day.toString();
-		dayDiv.classList.add('border-l', 'py-1');
-		dayDiv.style.width = `${dayWidth}px`;
-		dayRow.appendChild(dayDiv);
-		dayWidth = 0;
-	}
-
-	if (gantt.pixelsPerHour >= 24) {
-		const hourDiv = document.createElement('div');
-
-		hourDiv.innerText = newDate.hour.toString();
-		hourDiv.classList.add('border-l', 'py-1');
-		hourDiv.style.width = `${gantt.pixelsPerHour}px`;
-		hourRow.appendChild(hourDiv);
-	}
-
-	dayWidth += gantt.pixelsPerHour;
-	weekWidth += gantt.pixelsPerHour;
-	monthWidth += gantt.pixelsPerHour;
-	previousDate = newDate;
-}
-
-dateDiv?.appendChild(monthRow);
-dateDiv?.appendChild(weekRow);
-dateDiv?.appendChild(dayRow);
-
-if (gantt.pixelsPerHour >= 24) {
-	dateDiv?.appendChild(hourRow);
-}
-
-const now = DateTime.now();
-const nowX = Math.floor(
-	now.diff(gantt.horizon.start, 'hours').hours * gantt.pixelsPerHour,
-);
-
-ctx.strokeStyle = '#FF3366';
-ctx.lineWidth = 2;
-ctx.beginPath();
-ctx.moveTo(nowX, 0);
-ctx.lineTo(nowX, ganttCanvas.height);
-ctx.stroke();
-
-const nowDot = document.createElement('div');
-nowDot.classList.add('now-dot');
-nowDot.style.marginLeft = `${nowX - 7.5}px`;
-dateDiv.appendChild(nowDot);
+gantt.generateWorkers().render();
 
 let dragging = null as HTMLDivElement | null;
 
@@ -252,6 +94,8 @@ function ganttItemClick(e: MouseEvent) {
 	selectedItems.push(target);
 }
 
+const ganttDiv = document.getElementById('ganttDiv') as HTMLDivElement;
+
 for (const r of gantt.rows) {
 	const rowDiv = document.createElement('div');
 	rowDiv.id = `worker-${r.worker.id}`;
@@ -268,15 +112,8 @@ for (const r of gantt.rows) {
 
 	for (const t of r.worker.tasks) {
 		const taskDiv = document.createElement('div');
-		const start =
-			112 +
-			Math.floor(
-				t.startDate.diff(gantt.horizon.start, 'hours').hours *
-					gantt.pixelsPerHour,
-			);
-		const length = Math.floor(
-			t.endDate.diff(t.startDate, 'hours').hours * gantt.pixelsPerHour,
-		);
+		const start = 112 + Math.floor(t.startDate.diff(gantt.horizon.start, 'hours').hours * gantt.pixelsPerHour);
+		const length = Math.floor(t.endDate.diff(t.startDate, 'hours').hours * gantt.pixelsPerHour);
 
 		taskDiv.style.marginLeft = `${start}px`;
 		taskDiv.style.width = `${length}px`;
@@ -285,19 +122,14 @@ for (const r of gantt.rows) {
 		taskDiv.ondragstart = dragstartHandler;
 		taskDiv.draggable = true;
 
-		taskDiv.classList.add(
-			'gantt-item',
-			'absolute',
-			'rounded',
-			'my-1',
-			'text-slate-900',
-			'hover:cursor-move',
-		);
+		taskDiv.classList.add('gantt-item', 'absolute', 'rounded', 'my-1', 'text-slate-900', 'hover:cursor-move');
 		taskDiv.innerText = t.name;
 
 		rowDiv.appendChild(taskDiv);
 	}
 
 	rowDiv.appendChild(label);
+
 	ganttDiv.appendChild(rowDiv);
 }
+
